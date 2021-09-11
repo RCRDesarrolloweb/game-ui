755ba79800010ae947e8f411fd94aff92d86e067
RCRDesarrolloWeb <contreras06rj@gmail.com>
2021-09-11 19:01:44 -0400

    New Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-icon name="sports_esports" color="cyan-14" style="font-size:70vw" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
